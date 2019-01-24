ee2937db1f923b36478f29dc5ff2d4c5c7d416ec
JagadeeshGandepalli <JagadeeshGandepalli@DESKTOP-K485S34>
2019-01-24 14:40:50 +0530

    first project commit

---
+++ actuatorexample/src/main/java/com/example/actuatorexample/ActuatorexampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.actuatorexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorexampleApplication.class, args);
+	}
+
+}
+
